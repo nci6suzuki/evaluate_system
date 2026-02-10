0f61fadf538d38583c62ff873a6e759eb88ebd71
suzukimasahiro <m.suzuki@nci6.com>
2026-02-10 10:59:50 +0900

    修正

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { createServerClient } from "@supabase/ssr";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   let res = NextResponse.next({
     request: req,
   });
